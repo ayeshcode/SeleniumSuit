e78689d6537d207638fb58582c0a395b2031a3ca
Ayeshcode <test@gmail.com>
2020-06-28 18:17:56 +0530

    Add project files.

---
+++ SeleniumSuit/DatabaceSet.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.TeamFoundation.TestManagement.WebApi;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Principal;
+
+namespace SeleniumSuit
+{
+    class DatabaceSet
+    {
+    private static string applicationName = string.Empty;
+        public static SqlConnection connection;
+
+        public static SqlConnection GetConnection()
+        {
+            if (DatabaceSet.connection != null)
+            {
+                if (DatabaceSet.connection.State == ConnectionState.Open)
+                    return DatabaceSet.connection;
+                if (DatabaceSet.connection != null)
+                    DatabaceSet.connection.Dispose();
+                DatabaceSet.connection = (SqlConnection)null;
+            }
+            DatabaceSet.connection = new SqlConnection(ConfigurationManager.AppSettings["ConnectionString"]);
+            DatabaceSet.connection.Open();
+            return DatabaceSet.connection;
+        }
+
+       
+        public static void StoreInDB(
+          string Scenario,
+          string PageName,
+          string ElementName,
+          string TestDescription,
+          TestResult TestStatus)
+        {
+            SeleniumStatus seleniumStatus = new SeleniumStatus();
+            seleniumStatus.Scenario = Scenario + " - Dataset " + (object)Common.RowNumber;
+            seleniumStatus.PageName = PageName;
+            seleniumStatus.ElementName = ElementName;
+            seleniumStatus.TestDescription = TestDescription;
+            seleniumStatus.TestStatus = TestStatus.ToString();
+            seleniumStatus.UserName = WindowsIdentity.GetCurrent().Name;
+            seleniumStatus.TestID = ExecuteTest.TestID;
+            seleniumStatus.ParentID = "";
+            if (Common.Screenshot == null || TestStatus.ToString() == "Success")
+            {
+                seleniumStatus.Screenshot = " ";
+                seleniumStatus.FailureURL = " ";
+            }
+            else
+            {
+                seleniumStatus.Screenshot = Common.Screenshot;
+                seleniumStatus.FailureURL = Common.FailureURL;
+            }
+            Guid guid = TestRun.GetAll().Where<TestRun>((Func<TestRun, bool>)(run => run.ExecutedTime.ToString() == ExecuteTest.scenarioTime.ToString())).Select<TestRun, Guid>((Func<TestRun, Guid>)(run => run.TestRunID)).FirstOrDefault<Guid>();
+            seleniumStatus.TestRunID = guid;
+            Persistance.Create((object)seleniumStatus);
+        }
+
+       
+
+       
+
+        
+
+        private static DataTable GetDatabases(SqlConnection conn)
+        {
+            SqlCommand command = conn.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = "sp_databases";
+            DataTable dataTable = new DataTable();
+            dataTable.Load((IDataReader)command.ExecuteReader());
+            return dataTable;
+        }
+    }
+}
+
+    
+
---
+++ SeleniumSuit/Test.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Firefox;
+using System.Data.SqlClient;
+
+namespace SeleniumSuit
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void CromMethod()
+        {
+            string ActualResult;
+            string ExpectedResult = "Guru99 Bank Home Page";
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://demo.guru99.com/V4/");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.Name("uid")).SendKeys("uid");
+            driver.FindElement(By.Name("password")).SendKeys("password");
+            driver.FindElement(By.Name("btnLogin")).Click();
+            ActualResult = driver.Title;
+            if (ActualResult.Contains(ExpectedResult))
+            {
+                SqlConnection conn = new SqlConnection();
+                conn.ConnectionString = "Server=[server_name];Database=[database_name];Trusted_Connection=true";
+                conn.Open();
+                SqlCommand insertCommand = new SqlCommand("INSERT INTO Status (Status, Scenario, TestDescription) VALUES (@1, @3, @2)", conn);
+
+
+                insertCommand.Parameters.Add(new SqlParameter("1", "Pass"));
+                insertCommand.Parameters.Add(new SqlParameter("2", DateTime.Now));
+                insertCommand.Parameters.Add(new SqlParameter("3", "QA1"));
+
+          
+
+                Console.WriteLine("Commands executed! Total rows affected are " + insertCommand.ExecuteNonQuery());
+                Console.ReadLine();
+                Assert.IsTrue(true, "Test Case passed");
+            }
+            else
+           {
+               SqlConnection conn = new SqlConnection();
+              conn.ConnectionString = "Server=[server_name];Database=[database_name];Trusted_Connection=true";
+                conn.Open();
+                SqlCommand insertCommand = new SqlCommand("INSERT INTO Status (Status, Scenario, TestDescription) VALUES (@1, @3, @2)", conn);
+              insertCommand.Parameters.Add(new SqlParameter("1", "Fail"));
+               insertCommand.Parameters.Add(new SqlParameter("2", DateTime.Now));
+              insertCommand.Parameters.Add(new SqlParameter("3", "QA1"));
+
+            }
+
+
+
+            driver.Close();
+            driver.Quit();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
